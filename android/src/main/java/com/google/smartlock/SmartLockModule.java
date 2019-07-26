f45cc5cb39689ed92342b3bb10a6b9c17e49c15a
gerard sychay <hellogerard@gmail.com>
2019-07-26 15:11:27 -0400

    Revert "android.support -> androidx" (whoops, not yet)
    This reverts commit 8040f1b6ac238991d3d9bf5d98e5741d50c9ba82.

--- android/src/main/java/com/google/smartlock/SmartLockModule.java
+++ android/src/main/java/com/google/smartlock/SmartLockModule.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
 import android.content.IntentSender;
-import androidx.annotation.NonNull;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
 import android.net.Uri;
